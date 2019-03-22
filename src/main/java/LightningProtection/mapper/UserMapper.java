162ac0a873ceb187ee7ec76fb9c61df8a56bdc4f
YLQ <2465266566@qq.com>
2019-03-22 16:08:43 +0800

    用户登录接口

---
+++ src/main/java/LightningProtection/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package LightningProtection.mapper;
+
+import LightningProtection.pojo.ta_auth_user;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface UserMapper {
+    List<ta_auth_user> userLogin(@Param("username") String username, @Param("password") String password);
+    int userRegister(ta_auth_user adduser);
+}
